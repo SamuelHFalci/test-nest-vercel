44d6e6041019a2dc244c919bf3de5bab526d4cea
samuelhfalci <samuelhfalci@gmail.com>
2023-01-30 10:57:57 -0300

    removing build

--- api/test/index.ts
+++ api/test/index.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { TestModule } from '../../src/test/test.module';
 import { AppModule } from '../../src/app.module';
-import { TestService } from 'src/test/test.service';
+import { TestService } from '../../src/test/test.service';
 export default async function handler(request: any, response: any) {
   // Find record on dynamoDB for preventing duplications
 
   const app = await NestFactory.create(AppModule);
   const testService = await app.select(TestModule).resolve(TestService);
   const resp = await testService.findAll();
-  response.end(resp);
+  response.send(resp);
 }
